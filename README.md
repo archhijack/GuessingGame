6c384b5cbc67fb3d96b51b87acd2f36e2f52a1e1
Archisman Bhattacharya <archhijack@gmail.com>
2020-07-18 15:10:21 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## The Unix Workbench Peer Graded Assignment
+-n 
+**Make date**: 
+Sat Jul 18 15:05:08 IST 2020
+-n 
+**Number of lines in guessinggame.sh:** 
+23
---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "************************ Guessing Game *************************"
+function game {
+	read -p "Guess the number of files in current directory: " guess
+    files=$(ls -1 | wc -l)
+}
+game
+while [[ $guess -ne $files ]]
+do
+	if [[ $guess -lt $files ]] 
+	then
+	    echo "That is way low. Try again. "
+	else
+	    echo "That is way high. Try again. "
+	fi
+	game
+done
+echo " "
+echo "**********Congratulations**********"
+echo "You did it!"
+echo "Here are the files: " 
+ls -1
